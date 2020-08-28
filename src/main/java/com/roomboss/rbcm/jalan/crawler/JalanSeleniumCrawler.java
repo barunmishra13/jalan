c8d6e3ab0242f11c2eec697e49cd494c477167e0
barun <barun.misrha@roomboss.co.jp>
2020-08-28 15:16:00 +0900

    Modifications

--- src/main/java/com/roomboss/rbcm/jalan/crawler/JalanSeleniumCrawler.java
+++ src/main/java/com/roomboss/rbcm/jalan/crawler/JalanSeleniumCrawler.java
@@ -8,9 +8,10 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -20,19 +21,19 @@ public class JalanSeleniumCrawler {
     public static final String USR = "Y64351";
     public static final String PASS = "";
 
-    private static final Map<String, String> caps = new HashMap<>();
+    private static final TreeMap<LocalDate, Integer> caps = new TreeMap<>();
     private static final String JALAN_SCREENSHOTS_UBUNTU = "/home/barun/temp/screenshots/";
+    private static final String CHROME_DRIVER_PATH_UBUNTU = "/usr/bin/chromedriver";
     private static final String JALAN_SCREENSHOTS_MAC = "/Users/barunmishra/temp/screenshots/";
+    private static final String CHROME_DRIVER_PATH_MAC = "/usr/local/Caskroom/chromedriver/85.0.4183.83/chromedriver";
 
     static {
-        caps.put("2020/12/01","0");
-        caps.put("2020/12/31","0");
+        caps.put(LocalDate.parse("2020-12-01"),1);
+        caps.put(LocalDate.parse("2020-12-31"),1);
     }
 
     public void crawl() throws IOException {
-//        String chromeDriverPath = "/usr/local/Caskroom/chromedriver/85.0.4183.83/chromedriver" ;
-        String chromeDriverPath = "/usr/bin/chromedriver" ;
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH_UBUNTU);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors", "--silent");
         WebDriver driver = new ChromeDriver(options);
@@ -55,44 +56,94 @@ public class JalanSeleniumCrawler {
 
     private void setAvailability(WebDriver driver) {
         driver.findElement(By.className("adjustment")).click();
-//        validateIfCapsRange();
-        AtomicInteger capCounter = new AtomicInteger();
-        caps.forEach((date, val) -> {
-            capCounter.getAndIncrement();
-            String[] d = date.split("/");
-            String year = d[0];
-            String month = d[1];
-            String day = d[2];
-            Select seleYear = new Select(driver.findElement(By.name("changeFromYear")));
-            seleYear.selectByValue(year);
-            Select seleMonth = new Select(driver.findElement(By.name("changeFromMonth")));
-            seleMonth.selectByValue(String.valueOf(Integer.parseInt(month)));
-            Select seleDay = new Select(driver.findElement(By.name("changeFromDay")));
-            seleDay.selectByValue(String.valueOf(Integer.parseInt(day)-1));
-            driver.findElement(By.cssSelector(" img[alt='切替']")).click();
-            String path = ".//a[contains(@onclick, '"+date+"')]";
-        highLighterMethod(driver,driver.findElement(By.xpath(".//a[contains(@onclick, '2021/03/30')]")));
-//            String path = ".//a[contains(@onclick, '"+date+"')]";
-            driver.findElement(By.xpath(path)).click();
-            String mainWindow=driver.getWindowHandle();
-            Set<String> s1=driver.getWindowHandles();
-            for (String childWindow : s1) {
-                if (!mainWindow.equalsIgnoreCase(childWindow)) {
-                    driver.switchTo().window(childWindow);
-                    driver.findElement(By.name("supplyRoomCntString")).clear();
-                    driver.findElement(By.name("supplyRoomCntString")).sendKeys(val);
-                    driver.findElement(By.cssSelector(" img[title='変更内容を確認する']")).click();
-                    driver.findElement(By.cssSelector(" img[alt='変更する']")).click();
-                    driver.switchTo().window(mainWindow);
-                    try {
-                        this.takeScreenshot(driver, JALAN_SCREENSHOTS_UBUNTU + "s" +capCounter+".png");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        if(isCapsRange(caps)) {
+            driver.findElement(By.cssSelector(" img[alt='室数カレンダー']")).click();
+            setCapsForMonth(driver,caps.firstKey().getYear(), caps.firstKey().getMonth().getValue(), caps.firstEntry().getValue());
+        }
+        else {
+            AtomicInteger capCounter = new AtomicInteger();
+            caps.forEach((date, val) -> {
+                String dateString = date.toString();
+                capCounter.getAndIncrement();
+                selectSearchDate(driver, dateString);
+                String path = ".//a[contains(@onclick, '"+date+"')]";
+//          highLighterMethod(driver,driver.findElement(By.xpath(".//a[contains(@onclick, '2021/03/30')]")));
+                driver.findElement(By.xpath(path)).click();
+                setCapsForDate(driver, capCounter, val);
+            });
+        }
+
+
+    }
+
+    private boolean isCapsRange(TreeMap<LocalDate, Integer> caps) {
+        return caps.size() == 2
+                && caps.firstKey().getMonth() == caps.lastKey().getMonth()
+                && caps.firstKey().getDayOfMonth() == 1 && caps.lastKey().getDayOfMonth() >= 28
+                && caps.firstEntry().getValue().equals(caps.lastEntry().getValue());
+    }
+
+    private void setCapsForMonth(WebDriver driver, Integer year, Integer month, Integer val) {
+        String mainWindow= driver.getWindowHandle();
+        Set<String> s1= driver.getWindowHandles();
+        for (String childWindow : s1) {
+            if (!mainWindow.equalsIgnoreCase(childWindow)) {
+                driver.switchTo().window(childWindow);
+                Select selectYear = new Select(driver.findElement(By.name("year")));
+                selectYear.selectByValue(year.toString());
+                Select seleMonth = new Select(driver.findElement(By.name("month")));
+                seleMonth.selectByValue(month.toString());
+                driver.findElement(By.cssSelector(" img[alt='切替']")).click();
+                driver.switchTo().alert().accept();
+                driver.findElements(By.name("remRoomCnt"))
+                        .forEach(capsTextBox -> {
+                            capsTextBox.clear();
+                            capsTextBox.sendKeys(val.toString());
+                        });
+                driver.findElement(By.cssSelector(" img[title='変更内容を確認する']")).click();
+                driver.findElement(By.cssSelector(" img[alt='変更する']")).click();
+                driver.switchTo().window(mainWindow);
+                try {
+                    this.takeScreenshot(driver, JALAN_SCREENSHOTS_UBUNTU + "s_month.png");
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-        });
+        }
+    }
 
+    private void setCapsForDate(WebDriver driver, AtomicInteger capCounter, Integer val) {
+        String mainWindow= driver.getWindowHandle();
+        Set<String> s1= driver.getWindowHandles();
+        for (String childWindow : s1) {
+            if (!mainWindow.equalsIgnoreCase(childWindow)) {
+                driver.switchTo().window(childWindow);
+                driver.findElement(By.name("supplyRoomCntString")).clear();
+                driver.findElement(By.name("supplyRoomCntString")).sendKeys(val.toString());
+                driver.findElement(By.cssSelector(" img[title='変更内容を確認する']")).click();
+                driver.findElement(By.cssSelector(" img[alt='変更する']")).click();
+                driver.switchTo().window(mainWindow);
+                try {
+                    this.takeScreenshot(driver, JALAN_SCREENSHOTS_UBUNTU + "s" + capCounter +".png");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void selectSearchDate(WebDriver driver, String date) {
+        String[] d = date.split("/");
+        String year = d[0];
+        String month = d[1];
+        String day = d[2];
+        Select seleYear = new Select(driver.findElement(By.name("changeFromYear")));
+        seleYear.selectByValue(year);
+        Select seleMonth = new Select(driver.findElement(By.name("changeFromMonth")));
+        seleMonth.selectByValue(String.valueOf(Integer.parseInt(month)));
+        Select seleDay = new Select(driver.findElement(By.name("changeFromDay")));
+        seleDay.selectByValue(String.valueOf(Integer.parseInt(day)-1));
+        driver.findElement(By.cssSelector(" img[alt='切替']")).click();
     }
 
     public void highLighterMethod(WebDriver driver, WebElement element){
